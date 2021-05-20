f3d71d19f0900f1df3be06eeda2f77fe24aa8ff6
“Nazar <nazar.ua87@gmail.com>
2021-05-20 14:05:45 +0300

    f

---
+++ src/main/java/ua/lviv/iot/spring/SpringLesson.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLesson {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLesson.class, args);
+	}
+
+}
